c73828c148659f427db46da5683538c5d53ac1ee
jgvictores <jgvictores@github.com>
2015-05-29 11:16:33 +0200

    init cv1

---
+++ programs/cv1/main.cpp
@@ -0,0 +1,29 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "Cv1.hpp"
+
+int main(int argc, char** argv) {
+
+    ResourceFinder rf;
+    rf.setVerbose(true);
+    rf.setDefaultContext("Cv1");
+    rf.setDefaultConfigFile("Cv1.ini");
+    rf.configure(argc, argv);
+
+    Cv1 mod;
+    if(rf.check("help")) {
+        return mod.runModule(rf);
+    }
+
+    printf("Run \"%s --help\" for options.\n",argv[0]);
+    printf("%s checking for yarp network... ",argv[0]);
+    fflush(stdout);
+    Network yarp;
+    if (!yarp.checkNetwork()) {
+        fprintf(stderr,"[fail]\n%s found no yarp network (try running \"yarpserver &\"), bye!\n",argv[0]);
+        return -1;
+    } else printf("[ok]\n");
+
+    return mod.runModule(rf);
+}
+
