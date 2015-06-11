2b3258ff8d65bc833bc9b1dde8df41fb7a40e2ad
jgvictores <jgvictores@github.com>
2015-06-11 17:41:52 +0200

    more optimized

--- programs/cv1ToRoot/PremultPorts.cpp
+++ programs/cv1ToRoot/PremultPorts.cpp
@@ -16,29 +16,29 @@ void PremultPorts::onRead(Bottle& b) {
         exit(1);  // case: other --> still not implemented
     }
 
-    yarp::sig::Matrix H_root_hip(4,4);
+    yarp::sig::Matrix H_root_hip(4,4);  // TRUNK
     H_root_hip.eye();
 
-    yarp::sig::Matrix H_hip_neck(4,4);
+    yarp::sig::Matrix H_hip_neck(4,4);  // fixed
     H_hip_neck.eye();
 
-    yarp::sig::Matrix H_neck_head(4,4);
+    yarp::sig::Matrix H_neck_head(4,4);  // HEAD
     H_neck_head.eye();
 
-    yarp::sig::Matrix H_head_rgb(4,4);
+    yarp::sig::Matrix H_head_rgb(4,4);  // fixed
     H_head_rgb.eye();
 
-    yarp::sig::Matrix H_rgb(4,4);
-    H_rgb.eye();
-    H_rgb(0,3) = b.get(0).asDouble();
-    H_rgb(1,3) = b.get(1).asDouble();
-    H_rgb(2,3) = b.get(2).asDouble();
+    yarp::sig::Matrix P_rgb(4,1);
+    P_rgb(0,1) = b.get(0).asDouble();
+    P_rgb(1,1) = b.get(1).asDouble();
+    P_rgb(2,1) = b.get(2).asDouble();
+    P_rgb(3,1) = 1.0;
 
-    yarp::sig::Matrix H_root = H_rgb;  // Needs all elems
+    yarp::sig::Matrix P_root = H_root_hip * H_hip_neck * H_neck_head * H_head_rgb * P_rgb;  // Needs all elems
     Bottle outB;
-    outB.addDouble(H_root(0,3));
-    outB.addDouble(H_root(1,3));
-    outB.addDouble(H_root(2,3));
+    outB.addDouble(P_root(0,1));
+    outB.addDouble(P_root(1,1));
+    outB.addDouble(P_root(2,1));
     outPort->write(outB);
 }
 
