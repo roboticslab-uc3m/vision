99fc7aec24a44c83cbcca7d9dd50975d5b7df0fe
jgvictores <jgvictores@github.com>
2015-06-11 16:24:42 +0200

    update possible cv1 commands

--- programs/cv1/SegmentorThread.hpp
+++ programs/cv1/SegmentorThread.hpp
@@ -39,7 +39,7 @@
 #define DEFAULT_MAX_NUM_BLOBS 2
 #define DEFAULT_MORPH_CLOSING 2
 #define DEFAULT_MORPH_OPENING 0
-#define DEFAULT_OUT_FEATURES "locX0 locY0 locZ0 area hue sat val angle aspectRatio rectangularity axisFirst axisSecond solidity hueStdDev satStdDev valStdDev"  // it's a bottle!!
+#define DEFAULT_OUT_FEATURES "mmX mmY mmZ"  // it's a bottle!!
 #define DEFAULT_OUT_FEATURES_FORMAT 0  // 0=bottled,1=minimal
 #define DEFAULT_OUT_IMAGE 1
 #define DEFAULT_RATE_MS 20
