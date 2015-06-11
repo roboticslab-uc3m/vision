fa185b7b56784ab4420ca52e366b7207b693d3bd
jgvictores <jgvictores@github.com>
2015-06-11 17:27:00 +0200

    tested, less out

--- programs/cv1ToRoot/PremultPorts.cpp
+++ programs/cv1ToRoot/PremultPorts.cpp
@@ -10,7 +10,7 @@ void PremultPorts::setOutPort(Port* _outPort) {
 /************************************************************************/
 
 void PremultPorts::onRead(Bottle& b) {
-    printf("[PremultPorts] Got %s\n", b.toString().c_str());
+    //printf("[PremultPorts] Got %s\n", b.toString().c_str());
     if(b.size() != 3) {
         fprintf(stderr,"[error] for now only parsing 3-double lists\n");
         exit(1);  // case: other --> still not implemented
@@ -22,9 +22,9 @@ void PremultPorts::onRead(Bottle& b) {
     H_rgb(1,3) = b.get(1).asDouble();
     H_rgb(2,3) = b.get(2).asDouble();
 
-    //printf("PremultH using custom H:\n%s\n",H->toString().c_str());
+    //printf("[PremultH] using custom H:\n%s\n",H->toString().c_str());
 
-    yarp::sig::Matrix H_root;// = (*H) * originalH;
+    yarp::sig::Matrix H_root = H_rgb;  // Needs all elems
     Bottle outB;
     outB.addDouble(H_root(0,3));
     outB.addDouble(H_root(1,3));
