34358c8a416db9b7dbff211ec2d633e272a11d3e
jgvictores <jgvictores@github.com>
2015-06-11 17:31:41 +0200

    more structure

--- programs/cv1ToRoot/PremultPorts.cpp
+++ programs/cv1ToRoot/PremultPorts.cpp
@@ -16,13 +16,23 @@ void PremultPorts::onRead(Bottle& b) {
         exit(1);  // case: other --> still not implemented
     }
 
+    yarp::sig::Matrix H_root_hip(4,4);
+    H_root_hip.eye();
+
+    yarp::sig::Matrix H_hip_neck(4,4);
+    H_hip_neck.eye();
+
+    yarp::sig::Matrix H_neck_head(4,4);
+    H_neck_head.eye();
+
+    yarp::sig::Matrix H_head_rgb(4,4);
+    H_head_rgb.eye();
+
     yarp::sig::Matrix H_rgb(4,4);
     H_rgb.eye();
     H_rgb(0,3) = b.get(0).asDouble();
     H_rgb(1,3) = b.get(1).asDouble();
     H_rgb(2,3) = b.get(2).asDouble();
-
-    //printf("[PremultH] using custom H:\n%s\n",H->toString().c_str());
 
     yarp::sig::Matrix H_root = H_rgb;  // Needs all elems
     Bottle outB;
