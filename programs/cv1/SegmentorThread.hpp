6ff02e5523b266f33648dfae74bf5fe15f850603
jgvictores <jcgvicto@gmail.com>
2016-02-21 16:19:18 +0100

    [cv1] and [cvFaces] refractor double quotes to angled brackets

--- programs/cv1/SegmentorThread.hpp
+++ programs/cv1/SegmentorThread.hpp
@@ -15,12 +15,12 @@
 
 #include <yarp/sig/all.h>
 
-#include "cv.h"
-//#include "highgui.h" // to show windows
+#include <cv.h>
+//#include <highgui.h> // to show windows
 
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include "TravisLib.hpp"
 
