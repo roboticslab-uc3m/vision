549316e86627736e954d6c6692ae176add5ea9c8
rsantos88 <rasantos@it.uc3m.es>
2016-10-17 16:47:48 +0200

    added functions: play() stop() and more improvements

--- libraries/HeadYarp/Espeak/Espeak.hpp
+++ libraries/HeadYarp/Espeak/Espeak.hpp
@@ -47,6 +47,10 @@ class Espeak : public yarp::dev::DeviceDriver {
 
         int16_t getPitch();
 
+        bool play();
+
+        bool stop();
+
         // -------- DeviceDriver declarations. Implementation in IDeviceImpl.cpp --------
 
         /**
@@ -129,11 +133,11 @@ class Espeak : public yarp::dev::DeviceDriver {
         espeak_POSITION_TYPE position_type;
         espeak_AUDIO_OUTPUT output;
         char *path;
-        int Buflength, Options;
+        int buflength, options;
         void* user_data;
-        t_espeak_callback *SynthCallback;
+        t_espeak_callback *synthCallback;
         espeak_PARAMETER Param;
-        unsigned int Size, position, end_position, flags, *unique_identifier;
+        unsigned int size, position, end_position, flags, *unique_identifier;
         const char* voice;
 };
 
