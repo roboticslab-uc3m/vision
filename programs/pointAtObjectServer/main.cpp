8dcd75e7b103d0208f59d27c1a1619e9da48a2db
jgvictores <jcgvicto@gmail.com>
2017-05-01 21:07:09 +0200

    improve format of main

--- programs/pointAtObjectServer/main.cpp
+++ programs/pointAtObjectServer/main.cpp
@@ -2,13 +2,14 @@
 
 #include "PointAtObjectServer.hpp"
 
-int main(int argc, char *argv[]) {
-
+int main(int argc, char *argv[])
+{
     printf("Run \"pointAtObjectServer --help\" for options.\n");
     printf("pointAtObjectServer checking for yarp network... ");
     fflush(stdout);
     yarp::os::Network yarp;
-    if (!yarp.checkNetwork()) {
+    if ( ! yarp.checkNetwork() )
+    {
         printf("[fail]\npointAtObjectServer found no yarp network (try running \"yarpserver &\"), bye!\n");
         return 1;
     } else printf("[ok]\n");
