4f2e38f8f5fa03d8b8288bf9653c91b44ded414d
Bartek Łukawski <bwmn.peter@gmail.com>
2019-06-28 21:27:53 +0200

    Fix row/column vs x/y assignments

--- programs/haarDetection2D/SegmentorThread.cpp
+++ programs/haarDetection2D/SegmentorThread.cpp
@@ -122,8 +122,8 @@ void SegmentorThread::run()
         const int pxX = objects[i].x + objects[i].width / 2;
         const int pxY = objects[i].y + objects[i].height / 2;
 
-        int centerX = inCvMat.rows / 2;
-        int centerY = inCvMat.cols / 2;
+        int centerX = inCvMat.cols / 2;
+        int centerY = inCvMat.rows / 2;
 
         int distance = std::sqrt(std::pow(pxX - centerX, 2) + std::pow(pxY - centerY, 2));
 
@@ -145,8 +145,8 @@ void SegmentorThread::run()
                     objects[i].width / 2, objects[i].height / 2);
 
             // scale centroids and fit into [-1, 1] range
-            double cX = 2.0 * pxX / inCvMat.rows - 1.0;
-            double cY = 2.0 * pxY / inCvMat.cols - 1.0;
+            double cX = 2.0 * pxX / inCvMat.cols - 1.0;
+            double cY = 2.0 * pxY / inCvMat.rows - 1.0;
 
             output.addFloat64(cX); // Points right
             output.addFloat64(cY); // Points down
