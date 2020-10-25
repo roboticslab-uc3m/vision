7fd6c4a071669105197c794b1ca309ba1ed19543
jgvictores <jcgvicto@gmail.com>
2020-10-25 12:07:52 +0100

    init testHaarDetector

---
+++ tests/testHaarDetector.cpp
@@ -0,0 +1,77 @@
+#include "gtest/gtest.h"
+
+#include <yarp/os/Property.h>
+
+#include <yarp/dev/Drivers.h>
+#include <yarp/dev/PolyDriver.h>
+
+#include <yarp/sig/Image.h>
+#include <yarp/sig/ImageDraw.h>
+
+#include <ColorDebug.h>
+
+#include "IDetector.hpp"
+
+namespace roboticslab
+{
+
+/**
+ * @ingroup vision-tests
+ * @brief Tests \ref ColorRegionDetector
+ */
+class HaarDetectorTest : public testing::Test
+{
+
+    public:
+        virtual void SetUp()
+        {
+            yarp::os::Property deviceOptions;
+            deviceOptions.put("device", "HaarDetector");
+            deviceOptions.put("algorithm", "redMinusGreen");
+
+            if(!detectorDevice.open(deviceOptions))
+            {
+                CD_ERROR("Failed to open HaarDetector device\n");
+                return;
+            }
+
+            if (!detectorDevice.view(iDetector))
+            {
+                CD_ERROR("Problems acquiring detector interface\n");
+                return;
+            }
+
+            yarpImage.resize(300,200);
+        }
+
+        virtual void TearDown()
+        {
+        }
+
+
+    protected:
+        roboticslab::IDetector *iDetector;
+        yarp::dev::PolyDriver detectorDevice;
+        yarp::sig::ImageOf<yarp::sig::PixelRgb> yarpImage;
+};
+
+TEST_F( HaarDetectorTest, HaarDetector1)
+{
+    yarpImage.zero();
+    yarp::sig::VectorOf<DetectedObject> detectedObjects;
+    iDetector->detect(yarpImage,detectedObjects);
+    ASSERT_EQ(detectedObjects.size(), 0);
+}
+
+TEST_F( HaarDetectorTest, HaarDetector2)
+{
+    yarpImage.zero();
+
+    /*yarp::sig::VectorOf<DetectedObject> detectedObjects;
+    iDetector->detect(yarpImage,detectedObjects);
+    ASSERT_EQ(detectedObjects.size(), 1);
+    ASSERT_NEAR(detectedObjects[0].cx(), yarpImage.width()/2, 2);
+    ASSERT_NEAR(detectedObjects[0].cy(), yarpImage.height()/2, 2);*/
+}
+
+}  // namespace roboticslab
