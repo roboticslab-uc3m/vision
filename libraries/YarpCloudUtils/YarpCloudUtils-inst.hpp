3f54bbb8c4f198ee909a49cbf916c4b56f8e6912
Bartek Łukawski <bwmn.peter@gmail.com>
2020-12-02 22:40:44 +0100

    Shorten template instantiations with a macro

--- libraries/YarpCloudUtils/YarpCloudUtils-inst.hpp
+++ libraries/YarpCloudUtils/YarpCloudUtils-inst.hpp
@@ -7,87 +7,89 @@
 #include <yarp/sig/PointCloud.h>
 #include <yarp/sig/Vector.h>
 
+#define YCU_INST_SIGNATURE(in, out) \
+    template bool meshFromCloud(const in &, out &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
+
 namespace roboticslab
 {
 
 namespace YarpCloudUtils
 {
 
-template bool meshFromCloud(const yarp::sig::PointCloudXY &, yarp::sig::PointCloudXY &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZ &, yarp::sig::PointCloudXY &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudNormal &, yarp::sig::PointCloudXY &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZRGBA &, yarp::sig::PointCloudXY &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZI &, yarp::sig::PointCloudXY &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::PointCloudXY &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormal &, yarp::sig::PointCloudXY &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::PointCloudXY &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXY, yarp::sig::PointCloudXY)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXY, yarp::sig::PointCloudXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXY, yarp::sig::PointCloudNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXY, yarp::sig::PointCloudXYZRGBA)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXY, yarp::sig::PointCloudXYZI)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXY, yarp::sig::PointCloudInterestPointXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXY, yarp::sig::PointCloudXYZNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXY, yarp::sig::PointCloudXYZNormalRGBA)
 
-template bool meshFromCloud(const yarp::sig::PointCloudXY &, yarp::sig::PointCloudXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZ &, yarp::sig::PointCloudXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudNormal &, yarp::sig::PointCloudXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZRGBA &, yarp::sig::PointCloudXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZI &, yarp::sig::PointCloudXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::PointCloudXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormal &, yarp::sig::PointCloudXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::PointCloudXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZ, yarp::sig::PointCloudXY)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZ, yarp::sig::PointCloudXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZ, yarp::sig::PointCloudNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZ, yarp::sig::PointCloudXYZRGBA)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZ, yarp::sig::PointCloudXYZI)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZ, yarp::sig::PointCloudInterestPointXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZ, yarp::sig::PointCloudXYZNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZ, yarp::sig::PointCloudXYZNormalRGBA)
 
-template bool meshFromCloud(const yarp::sig::PointCloudXY &, yarp::sig::PointCloudNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZ &, yarp::sig::PointCloudNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudNormal &, yarp::sig::PointCloudNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZRGBA &, yarp::sig::PointCloudNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZI &, yarp::sig::PointCloudNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::PointCloudNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormal &, yarp::sig::PointCloudNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::PointCloudNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
+YCU_INST_SIGNATURE(yarp::sig::PointCloudNormal, yarp::sig::PointCloudXY)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudNormal, yarp::sig::PointCloudXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudNormal, yarp::sig::PointCloudNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudNormal, yarp::sig::PointCloudXYZRGBA)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudNormal, yarp::sig::PointCloudXYZI)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudNormal, yarp::sig::PointCloudInterestPointXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudNormal, yarp::sig::PointCloudXYZNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudNormal, yarp::sig::PointCloudXYZNormalRGBA)
 
-template bool meshFromCloud(const yarp::sig::PointCloudXY &, yarp::sig::PointCloudXYZRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZ &, yarp::sig::PointCloudXYZRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudNormal &, yarp::sig::PointCloudXYZRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZRGBA &, yarp::sig::PointCloudXYZRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZI &, yarp::sig::PointCloudXYZRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::PointCloudXYZRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormal &, yarp::sig::PointCloudXYZRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::PointCloudXYZRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZRGBA, yarp::sig::PointCloudXY)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZRGBA, yarp::sig::PointCloudXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZRGBA, yarp::sig::PointCloudNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZRGBA, yarp::sig::PointCloudXYZRGBA)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZRGBA, yarp::sig::PointCloudXYZI)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZRGBA, yarp::sig::PointCloudInterestPointXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZRGBA, yarp::sig::PointCloudXYZNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZRGBA, yarp::sig::PointCloudXYZNormalRGBA)
 
-template bool meshFromCloud(const yarp::sig::PointCloudXY &, yarp::sig::PointCloudXYZI &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZ &, yarp::sig::PointCloudXYZI &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudNormal &, yarp::sig::PointCloudXYZI &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZRGBA &, yarp::sig::PointCloudXYZI &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZI &, yarp::sig::PointCloudXYZI &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::PointCloudXYZI &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormal &, yarp::sig::PointCloudXYZI &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::PointCloudXYZI &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZI, yarp::sig::PointCloudXY)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZI, yarp::sig::PointCloudXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZI, yarp::sig::PointCloudNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZI, yarp::sig::PointCloudXYZRGBA)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZI, yarp::sig::PointCloudXYZI)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZI, yarp::sig::PointCloudInterestPointXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZI, yarp::sig::PointCloudXYZNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZI, yarp::sig::PointCloudXYZNormalRGBA)
 
-template bool meshFromCloud(const yarp::sig::PointCloudXY &, yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZ &, yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudNormal &, yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZRGBA &, yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZI &, yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormal &, yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
+YCU_INST_SIGNATURE(yarp::sig::PointCloudInterestPointXYZ, yarp::sig::PointCloudXY)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudInterestPointXYZ, yarp::sig::PointCloudXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudInterestPointXYZ, yarp::sig::PointCloudNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudInterestPointXYZ, yarp::sig::PointCloudXYZRGBA)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudInterestPointXYZ, yarp::sig::PointCloudXYZI)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudInterestPointXYZ, yarp::sig::PointCloudInterestPointXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudInterestPointXYZ, yarp::sig::PointCloudXYZNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudInterestPointXYZ, yarp::sig::PointCloudXYZNormalRGBA)
 
-template bool meshFromCloud(const yarp::sig::PointCloudXY &, yarp::sig::PointCloudXYZNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZ &, yarp::sig::PointCloudXYZNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudNormal &, yarp::sig::PointCloudXYZNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZRGBA &, yarp::sig::PointCloudXYZNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZI &, yarp::sig::PointCloudXYZNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::PointCloudXYZNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormal &, yarp::sig::PointCloudXYZNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::PointCloudXYZNormal &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormal, yarp::sig::PointCloudXY)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormal, yarp::sig::PointCloudXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormal, yarp::sig::PointCloudNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormal, yarp::sig::PointCloudXYZRGBA)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormal, yarp::sig::PointCloudXYZI)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormal, yarp::sig::PointCloudInterestPointXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormal, yarp::sig::PointCloudXYZNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormal, yarp::sig::PointCloudXYZNormalRGBA)
 
-template bool meshFromCloud(const yarp::sig::PointCloudXY &, yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZ &, yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudNormal &, yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZRGBA &, yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZI &, yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudInterestPointXYZ &, yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormal &, yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
-template bool meshFromCloud(const yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::PointCloudXYZNormalRGBA &, yarp::sig::VectorOf<int> &, const yarp::os::Searchable &);
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormalRGBA, yarp::sig::PointCloudXY)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormalRGBA, yarp::sig::PointCloudXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormalRGBA, yarp::sig::PointCloudNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormalRGBA, yarp::sig::PointCloudXYZRGBA)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormalRGBA, yarp::sig::PointCloudXYZI)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormalRGBA, yarp::sig::PointCloudInterestPointXYZ)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormalRGBA, yarp::sig::PointCloudXYZNormal)
+YCU_INST_SIGNATURE(yarp::sig::PointCloudXYZNormalRGBA, yarp::sig::PointCloudXYZNormalRGBA)
 
 } // YarpCloudUtils
 
 } // namespace roboticslab
-
 
 #endif // __YARP_CLOUD_UTILS_INST_HPP__
