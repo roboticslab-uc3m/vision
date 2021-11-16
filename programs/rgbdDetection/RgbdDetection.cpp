74f87564ea670212d9dffda8d256432b504d1ece
Bartek Łukawski <bwmn.peter@gmail.com>
2021-11-16 13:55:49 +0100

    Use list initializer for tuple

--- programs/rgbdDetection/RgbdDetection.cpp
+++ programs/rgbdDetection/RgbdDetection.cpp
@@ -219,8 +219,7 @@ bool RgbdDetection::updateModule()
 
             if (depth > 0.0f)
             {
-                records.push_back(std::make_tuple(depth, pxDepth, pyDepth,
-                                                  pxColor, pyColor, (brx - tlx) / 2, (bry - tly) / 2));
+                records.push_back({depth, pxDepth, pyDepth, pxColor, pyColor, (brx - tlx) / 2, (bry - tly) / 2});
 
                 if (!closest || depth < std::get<0>(*closest))
                 {
