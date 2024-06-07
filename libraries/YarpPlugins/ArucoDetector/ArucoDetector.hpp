aebfc45958b32a441456430a924c32379d04285c
anacg1620 <anacg1620@gmail.com>
2024-06-07 14:02:11 +0200

    #111 add ArucoDetector hpp (not tested)

---
+++ libraries/YarpPlugins/ArucoDetector/ArucoDetector.hpp
@@ -0,0 +1,33 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef __ARUCO_DETECTOR_HPP__
+#define __ARUCO_DETECTOR_HPP__
+
+#include <yarp/dev/DeviceDriver.h>
+#include <opencv2/objdetect.hpp>
+#include "IDetector.hpp"
+
+namespace roboticslab
+{
+
+/**
+ * @ingroup YarpPlugins
+ * @defgroup ArucoDetector
+ * @brief Contains roboticslab::ArucoDetector.
+ */
+
+class ArucoDetector : public yarp::dev::DeviceDriver,
+                      public IDetector
+{
+public:
+    bool open(yarp::os::Searchable& config) override;
+    bool detect(const yarp::sig::Image& inYarpImg, yarp::os::Bottle& detectedObjects) override;
+
+private:
+    cv::aruco::DetectorParameters detectorParams;
+    cv::aruco::Dictionary dictionary;
+};
+
+}
+
+#endif // __ARUCO_DETECTOR_HPP__
